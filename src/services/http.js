de4bdbfaaa6b084d69fde1b5668657bff29e5656
rich <adukyerer@gmail.com>
2022-12-18 13:45:56 +0000

    Update link

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://rent-a-ride-backend.onrender.com/',
+  baseURL: 'https://rails-4sct.onrender.com',
   headers: {
     'Content-type': 'application/json',
     Authorization: localStorage.getItem('token'),
